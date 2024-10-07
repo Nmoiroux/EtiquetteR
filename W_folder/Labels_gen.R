3a21c6f1e2c1e6a5e60340b5214f804787538a9d
Nmoiroux <n.moiroux@gmail.com>
2024-10-07 16:52:17 +0000

    W_file

--- W_folder/Labels_gen.R
+++ W_folder/Labels_gen.R
@@ -1,15 +1,12 @@
-source("Functions_lab_gen.R") # load tidyverse and tools packages, as well as several functions used to print the labels
+#source("Functions_lab_gen.R") # load tidyverse and tools packages, as well as several functions used to print the labels
 
-# Exemple
-library(readODS) # I store my data in an OpenDocument Spreadsheet (libreoffice)
+# Example
+require(readODS) # I store my data in an Open Document Spreadsheet (libreoffice)
 
-path <- "liste_ind_coll_ex.ods" # define path (or file if it is in the same folder/env.)
+path <- "W_folder/liste_ind_coll_ex.ods" # define path (or file if it is in the same folder/env.)
 individuals <- readODS::read_ods(path, sheet = "Table_data") # Table of data to print (list of individuals for ex.) is stored in sheet "Table_data"
 print_informations <- readODS::read_ods(path, sheet = "Print_parameters_ex1") # printing parameters (to be defined by the user)
-file_out <- "labels_mozzie.tex" # Name of the latex file to write (the same name will be used for the pdf file)
-lab_size <- 20 # label size (in mm)
-n_col <- 7 # number of column of labels per pages
-col_N_name <- "N" # name of the column in the table that indicates the number of individuals per row.
-hl_col <- "orange"# color to be used for text highlighting
+file_out <- "W_folder/labels_mozzie.tex" # Name of the latex file to write (the same name will be used for the pdf file)
 
 create_pdf(file_out = file_out,  ind_list = individuals , print_info = print_informations, lab_size = 15, n_col = 8, col_N_name = "N", hl_col = "orange") # Write a Latex file and create corresponding pdf
+
