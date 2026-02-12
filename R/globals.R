6bd4a2b2151ab35c5a25f7fc44e47dcf37ba9a08
Nmoiroux <n.moiroux@gmail.com>
2026-02-12 16:36:11 +0000

    add globals var

---
+++ R/globals.R
@@ -0,0 +1,2 @@
+utils::globalVariables(c("data","field_name", "label_no", "line_break", "order_lab", "prefix", "print_opt_hl",
+    "print_opt_it", "print_opt_par", "print_sex_symbol", "print_txt"))
