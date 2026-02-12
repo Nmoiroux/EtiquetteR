acd7d1d214839ac77b938e129f7d9485b136d911
Nmoiroux <n.moiroux@gmail.com>
2026-02-12 16:35:50 +0000

    truncated line in doc

--- R/Functions_lab_gen.R
+++ R/Functions_lab_gen.R
@@ -342,7 +342,15 @@ print_bottom <- function(file_out){
 #'
 #' @examples
 #' # Example usage:
-#' # create_pdf("output.tex", ind_list, print_info, lab_width = 15, lab_height = 9, font_size = 4, n_col = 8, col_N_name = "N", hl_col = "orange")
+#' # create_pdf("output.tex", 
+#'   ind_list, 
+#'   print_info, 
+#'   lab_width = 15, 
+#'   lab_height = 9, 
+#'   font_size = 4, 
+#'   n_col = 8, 
+#'   col_N_name = "N", 
+#'   hl_col = "orange")
 #' @export
 
 create_pdf <- function(file_out, ind_list, print_info,lab_width = 15, lab_height = 9, font_size = 4, n_col = 8, col_N_name = NA, hl_col = "orange"){
